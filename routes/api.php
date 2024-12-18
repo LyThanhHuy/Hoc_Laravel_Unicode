f617e27f656eaadb8d0e0192b0db778be2233a81
Huy Ly <huyly@co-pro.vn>
2024-12-18 09:58:38 +0700

    Video 21

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\ProductsController;
+// use App\Http\Controllers\Api\ProductsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('products', [ProductsController::class, 'getAllProducts']);
+// Route::get('products', [ProductsController::class, 'getAllProducts']);
--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,9 @@ Route::get('/san-pham', [HomeController::class, 'products'])->name('product');
 
 Route::get('/them-san-pham', [HomeController::class, 'getAdd']);
 
-// Route::post('/them-san-pham', [HomeController::class, 'postAdd']);
+Route::post('/them-san-pham', [HomeController::class, 'postAdd']);
 
-Route::put('/them-san-pham', [HomeController::class, 'putAdd']);
+// Route::put('/them-san-pham', [HomeController::class, 'putAdd']);
 
 Route::get('lay-thong-tin', [HomeController::class, 'getArr']);
 
